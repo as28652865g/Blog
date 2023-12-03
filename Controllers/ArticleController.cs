a5a2f028704dd1bc43216bcf59b2a20f2d063993
Pinhan Chen <as28652865g@gmail.com>
2023-12-03 20:02:56 +0800

    Dev：環境建置

---
+++ Controllers/ArticleController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EFBlog.Controllers
+{
+    public class ArticleController : Controller
+    {
+        [HttpGet("Article/{id}")]
+        public async Task<IActionResult> Index(long id)
+        {
+            return View();
+        }
+
+        [HttpGet("CreateArticle")]
+        public IActionResult CreateArticle()
+        { 
+            return View(); 
+        }
+
+        [HttpGet("GetArticleList")]
+        public async Task<IActionResult> GetArticleList()
+        {
+            return View();
+        }
+
+        [HttpGet("UpdateArticle/{id}")]
+        public async Task<IActionResult> UpdateArticle(long id)
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+builder.Services.AddControllersWithViews();
 
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
 app.Run();
